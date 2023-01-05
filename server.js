6613a52fed229ce7c85087e2fc01fdd7340d2baa
Quintin Xu <xuqunlele@gmail.com>
2023-01-05 23:00:04 +1100

    1. remove unused codes

--- server.js
+++ server.js
@@ -11,56 +11,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/projects", projectRoutes);
 
-// const createCollection = (collectionName) => {
-//   client.connect((err, db) => {
-//     projectCollection = client.db().collection(collectionName);
-
-//     if (!err) {
-//       console.log("MongoDB connected");
-//     } else {
-//       console.log("MongoDB connection error");
-//       process.exit(1);
-//     }
-//   });
-// };
-
-// const insertProjects = (project, callback) => {
-//   projectCollection.insertOne(project, callback);
-// };
-
-// const getProjects = (callback) => {
-//   projectCollection.find({}).toArray(callback);
-// };
-
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
-
-// app.get("/api/projects", (req, res) => {
-//   getProjects((err, result) => {
-//     if (err) {
-//       res.json({ statusCode: 400, message: err });
-//     } else {
-//       res.json({ statusCode: 200, data: result, message: "Success" });
-//     }
-//   });
-// });
-
-// app.post("/api/projects", (req, res) => {
-//   const newProject = req.body;
-//   insertProjects(newProject, (err, result) => {
-//     console.log("A new project is added:", newProject);
-//     if (err) {
-//       res.json({ statusCode: 400, message: err });
-//     } else {
-//       res.json({ statusCode: 200, data: result, message: "Success" });
-//     }
-//   });
-// });
 
 var port = process.env.port || 3000;
 
 app.listen(port, () => {
   console.log("App listening to: " + port);
-  // createCollection("Pets");
 });
